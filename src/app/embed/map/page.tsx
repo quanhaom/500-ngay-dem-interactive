4708c2c7a41398ab6e131131d14e0b912573a0f1
quanhaom <quan67100@gmail.com>
2026-09-24 22:46:15 +0700

    Fix transparent Wix map embed

--- src/app/embed/map/page.tsx
+++ src/app/embed/map/page.tsx
@@ -1,8 +1,50 @@
-import VietnamRemainsMap from "../../../components/map/VietnamRemainsMap";
+"use client";
 
+import { useEffect } from "react";
+
+import VietnamRemainsMap from "../../../components/map/VietnamRemainsMap";
 import styles from "./page.module.css";
 
 export default function EmbeddedMapPage() {
+  useEffect(() => {
+    const html = document.documentElement;
+    const body = document.body;
+
+    const oldHtmlBackground =
+      html.style.background;
+
+    const oldBodyBackground =
+      body.style.background;
+
+    const oldHtmlColorScheme =
+      html.style.colorScheme;
+
+    html.style.setProperty(
+      "background",
+      "transparent",
+      "important"
+    );
+
+    body.style.setProperty(
+      "background",
+      "transparent",
+      "important"
+    );
+
+    html.style.colorScheme = "normal";
+
+    return () => {
+      html.style.background =
+        oldHtmlBackground;
+
+      body.style.background =
+        oldBodyBackground;
+
+      html.style.colorScheme =
+        oldHtmlColorScheme;
+    };
+  }, []);
+
   return (
     <main className={styles.page}>
       <VietnamRemainsMap
